1d7d74807c30d92071dba797943e8ae5881c8a9d
rupinder <inforupinder79@gmail.com>
2023-10-13 14:29:54 +0530

    initial commit

---
+++ src/data/BlogData.js
@@ -0,0 +1,12 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
+        tags:["react","styled-components"],
+        date:"10 Oct, 2023",
+        imgSrc:"https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
+        link:"#"
+    }
+] 
